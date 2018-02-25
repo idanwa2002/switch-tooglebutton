09a1f830f1d06ba524c22cb2ec31b1bf2fc961ed
idanwa2002 <av5307@bs.amalnet.k12.il>
2018-02-25 23:52:15 +0200

    switch and tgbtn

--- app/src/main/java/com/example/idan/idanwasserstb/MainActivity.java
+++ app/src/main/java/com/example/idan/idanwasserstb/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.idan.idanwasserstb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.ToggleButton;
+import android.graphics.Color;
 public class MainActivity extends AppCompatActivity {
-
+ToggleButton tb;
+Switch sw;
+LinearLayout lil;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tb = (ToggleButton) findViewById(R.id.tob);
+        sw = (Switch) findViewById(R.id.swi);
+        lil = (LinearLayout) findViewById(R.id.ll);
+    }
+    public void click (View view){
+        if (sw.isChecked()&&(tb.isChecked()))
+            lil.setBackgroundColor(Color.GREEN);
+        if (sw.isChecked()&&!(tb.isChecked()))
+            lil.setBackgroundColor(Color.RED);
+        if (!(sw.isChecked())&&(tb.isChecked()))
+            lil.setBackgroundColor(Color.BLUE);
+        if (!(sw.isChecked())&&!(tb.isChecked()))
+            lil.setBackgroundColor(Color.WHITE);
     }
 }
 
